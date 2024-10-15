79a712ac5cfc562395fc38920e41f147fc69e8e1
Igor Flores <contato.ofloress999@gmail.com>
2024-10-15 13:54:34 -0300

    Add files via upload

---
+++ Atividade_C#/Exercicio_1.cs
@@ -0,0 +1,30 @@
+﻿//Adiciona a biblioteca System
+using System;
+
+
+namespace exercicio1;
+public class Exercicio_1
+{
+    static void ex_1()
+    {
+        
+        Console.WriteLine("Escreva o primeiro texto: "); //Pede para o usuario entrar com um texto
+        var text1 = Console.ReadLine(); //Guarda o valor fornecido pelo usuario
+        Console.WriteLine("Escreva o segundo texto: ");
+        var text2 = Console.ReadLine();
+
+        if(string.Compare(text1.ToUpper(), text2.ToUpper()) == 0) //Utilizando o .ToUpper deixamos ambos textos em maisculo para comparar o .Compare e verificar se sao iguais;
+        {
+            Console.WriteLine("As strings são iguais.");
+        } else
+        {
+            Console.WriteLine("As strings são diferentes.");
+        }
+
+        Console.WriteLine($"O texto 1 tem {text1.Length} caracteres"); //Retornamos uma mensagem utilizando .Length para informar a quantidade de caracteres do texto.
+        Console.WriteLine($"O texto 2 tem {text2.Length} caracteres");
+
+
+    }
+
+}
---
+++ Atividade_C#/Exericio_2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace exercicio2;
+
+public class Exericio_2
+{
+    static void ex_2()
+    {
+        Console.WriteLine("Escreva uma palavra: ");
+        var text = Console.ReadLine();
+
+        Console.WriteLine("Escreva uma unica letra: ");
+        char letra = char.Parse(Console.ReadLine());
+
+        text = text.ToUpper();
+        letra = Char.ToUpper(letra);
+
+        int count = 0;
+
+        for (int i = 0;i < text.Length; i++)
+        {
+            if (text[i] == letra)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"A letra {letra} aparece {count} vezes");
+    }
+}
+
+/*
+ Escreva um programa que solicite ao usuário uma palavra (string) e uma letra(char). 
+O programa deve contar e exibir quantas vezes a letra aparece na palavra, 
+ignorando se está em maiúsculas ou minúsculas. 
+Para ler a variável letra do tipo char: 
+letra = Console.ReadLine()[0];       
+Para deixar maisculas:  palavra = palavra.ToUpper();        
+letra = Char.ToUpper(letra); 
+*/
